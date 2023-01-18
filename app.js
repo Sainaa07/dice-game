1ab61d4360b43d6054d6487145c0ddce5c075632
Sainaa <kolyasainbayar@gmail.com>
2023-01-18 22:56:46 +0800

    toglogchin eeljig solidog bolgono

--- app.js
+++ app.js
@@ -13,12 +13,26 @@ document.getElementById('score-0').textContent = "0";
 document.getElementById('score-1').textContent = "0"
 document.getElementById('current-0').textContent = "0";
 document.getElementById('current-1').textContent = "0";
-
+//shoo shideh eventlistener
 document.querySelector(".dice").style.display = "none";
+
 document.querySelector(".btn-roll").addEventListener("click",function(){
     var diceNumber = Math.floor(Math.random() * 6) + 1;
-
+    // 1 - 6 dotor rand tooo gargaj avna
     diceDom.style.display = "block";
     diceDom.src = "dice-" + diceNumber + ".png";
+    //toglogchin eelj solih
+    if (diceNumber !== 1) {
+        roundScore = roundScore + diceNumber;
+        document.getElementById("current-" + activePlayer).textContent = roundScore;
+    } else {
+        roundScore = 0;
+        document.getElementById("current-" + activePlayer).textContent = 0;
+        activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+        document.querySelector(".player-0-panel").classList.toggle("active");
+        document.querySelector(".player-1-panel").classList.toggle("active");
+        diceDom.style.display = "none";
+    }
 });
+
 
